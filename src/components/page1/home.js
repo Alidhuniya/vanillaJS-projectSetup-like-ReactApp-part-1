22c0a1a11cb51577b8cf05452c34f31e076e9f0c
Ali Dhuniya <39728698+Alidhuniya@users.noreply.github.com>
2020-06-23 00:26:54 -0700

    Update home.js

--- src/components/page1/home.js
+++ src/components/page1/home.js
@@ -7,7 +7,7 @@ const Home = () => {
    
     return `
     <nav>
-    <li><a href="/public/page2.html"> Page 2 </a></li>
+    <li><a href="https://alidhuniya.github.io/vanillaJS-projectSetup-like-ReactApp-part-1/public/page2.html"> Page 2 </a></li>
     </nav>
     <h3> page is ${hm}</h3>
     `
